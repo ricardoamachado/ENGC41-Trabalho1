607413a327355fbc888fda76e69041285793bef9
Ricardo M <thezsparkk@gmail.com>
2022-05-08 12:15:08 -0300

    Fim da questão 1

--- questao1.py
+++ questao1.py
@@ -1,11 +1,30 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
+plt.style.use('ggplot')
 def main():
     dados = pd.read_csv('questao1.txt',sep='\t')
     corrente_diodo = dados['I(D1)']
     tensao_diodo = dados['v1']
+
+    #Ajuste da caracteristica exponencial
+    corrente_diodo_ln = np.log(corrente_diodo)
+    d, c = np.polynomial.polynomial.polyfit(tensao_diodo,corrente_diodo_ln,1)
+
+    #Determinação dos valores de eta, Is e da corrente do modelo exponencial.
+    eta = 1/(0.026*c)
+    Is = np.exp(d)
+    corrente_diodo_exp = Is*np.exp(tensao_diodo/(0.026*eta))
+
+    #Print dos valores de eta e Is
+    print(f"Para o modelo exponencial, temos que eta:{eta:.3f} V e Is:{Is*10**9:.3f} nV.")
+
+    #Plot comparando a caracteristica simulada com o modelo exponencial.
+    plt.figure(1)
+    plt.plot(tensao_diodo,corrente_diodo_exp)
+    plt.plot(tensao_diodo,corrente_diodo)
+    plt.savefig('output1.png')
+
     #Fatiar o vetores de forma a pegar apenas o ultimo oitavo com a função array_split.
     #Detalhe: Em python os índices dos vetores começam em zero. Por isso, o índice 7 foi escolhido para pegar o ultimo oitavo.
     tensao_diodo_oitavo = np.array_split(tensao_diodo,8)[7]
@@ -20,12 +39,13 @@ def main():
     corrente_diodo_linear = tensao_diodo_oitavo/rd - Vd0/rd
 
     #Print dos valores de rd e Vd0
-    print(f"Para o modelo linearizado temos que rd:{rd:.3f} Ohm e Vd0:{Vd0:.3f} V.")
+    print(f"Para o modelo linearizado, temos que rd:{rd:.3f} Ohm e Vd0:{Vd0:.3f} V.")
 
-    #Plot preliminar. MUDAR DESCRIÇÃO DEPOIS.
+    #Plot preliminar. MUDAR DESCRIÇÃO DEPOIS
+    plt.figure(2)
     plt.plot(tensao_diodo_oitavo,corrente_diodo_linear)
     plt.plot(tensao_diodo,corrente_diodo)
-    plt.savefig('output1.png')
+    plt.savefig('output2.png')
     plt.show()
 
 if __name__ == "__main__":
